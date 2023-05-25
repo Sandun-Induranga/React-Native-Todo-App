2827bdf8738cae81f675ca7db58206a8de11fc4f
Sandun Induranga <sandutharu40@gmail.com>
2023-05-25 21:30:40 +0530

    Home Component Updated

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Home from "./screens/Home";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Text, StyleSheet, TouchableOpacity, View } from "react-native";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "darkblue", fontSize: 40, marginBottom: 60 }}>
+        Manage Todo List
+      </Text>
+      <TouchableOpacity style={styles.buttons} onPress={() => alert("Clicked")}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttons: {
+    backgroundColor: "darkblue",
+    borderColor: "white",
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 24,
+  },
+});
